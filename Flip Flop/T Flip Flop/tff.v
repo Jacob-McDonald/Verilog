6311d7a552c124a81e656d4f4b710a32cffe774f
Jacob McDonald <jacobmmcdonald@outlook.com>
2018-03-23 12:00:01 -0700

    Update tff.v

--- Flip Flop/T Flip Flop/tff.v
+++ Flip Flop/T Flip Flop/tff.v
@@ -1,19 +1,20 @@
-module tff(t,clk,rst, q,qb
-input t,clk,rst
-output reg q,qb
+module tff(
+
+input t,
+input clk,
+input rst,
+output reg q,
+output reg qn
 );
 
+always@ (posedge clk,posedge rst) begin
 
-
-always@(posedge clk,posedge rst) begin
-	if (!rst) 
-		q <= 1'b0
-
-	else begin
-		if(t)
-			q <= ~q;
-	end
-
+if (!rst) 
+	q <= 1'b0
+else begin
+	if(t)
+		q <= ~q;
+end
 
 end
 
